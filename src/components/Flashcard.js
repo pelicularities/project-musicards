172cf69a5a1cffc1b0ff7b9b4d7dda27afcc657a
Grace Teng <pelicularities@users.noreply.github.com>
2021-02-15 13:58:41 +0800

    Build and style Flashcard component
    - Add card flip functionality to Flashcard component
    - Install Material UI
    - Style Flashcard component using styled components

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -1,11 +1,33 @@
 import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+
+const useStyles = makeStyles({
+  flashcard: {
+    width: "14rem",
+    height: "18rem",
+    borderRadius: "0.25rem",
+    fontSize: "2rem",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    cursor: "pointer",
+  },
+});
 
 function Flashcard({ flashcard }) {
+  const classes = useStyles();
+  const { front, back } = flashcard;
+  const [isFront, setIsFront] = useState(true);
+
+  const flipCard = () => {
+    setIsFront(!isFront);
+  };
+
   return (
-    <div>
-      <p>Front: {flashcard.back}</p>
-      <p>Back: {flashcard.back}</p>
-    </div>
+    <Card className={classes.flashcard} variant="outlined" onClick={flipCard}>
+      {isFront ? front : back}
+    </Card>
   );
 }
 
