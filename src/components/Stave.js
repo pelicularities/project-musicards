316e3bee4954898418a63a6653d370dbcd625f8d
Grace Teng <pelicularities@users.noreply.github.com>
2021-02-18 14:49:07 +0800

    WIP: Add time signature logic (not just display)
    - Current time signature dropdown only changes the time signature
      that is rendered on the stave, but does not actually change the
      time signature of the underlying logic of the score
    - Number of notes must match score's time signature

--- src/components/Stave.js
+++ src/components/Stave.js
@@ -22,6 +22,7 @@ function Stave({
   const divRef = useRef(null);
   const classes = useStyles();
   let className = "";
+
   useLayoutEffect(() => {
     divRef.current.innerHTML = "";
     const vf = new Vex.Flow.Factory({
@@ -30,6 +31,36 @@ function Stave({
 
     const score = vf.EasyScore();
     const system = vf.System();
+
+    let defaultRest = "B4/1/r";
+    if (timeSignature) {
+      score.set({ time: timeSignature });
+      switch (timeSignature) {
+        case "2/4":
+          // this case works
+          defaultRest = "B4/2/r";
+          break;
+        case "3/4":
+          // does not work - shows note instead of rest
+          defaultRest = "B4/2./r";
+          break;
+        case "6/8":
+          // does not work
+          defaultRest = "B4/4./r, B4/4./r";
+          break;
+        case "9/8":
+          // does not work
+          defaultRest = "B4/4./r, B4/4./r, B4/4./r";
+          break;
+        case "12/8":
+          // does not work
+          defaultRest = "B4/4./r, B4/4./r, B4/4./r, B4/4./r";
+          break;
+        default:
+          // this case works
+          break;
+      }
+    }
 
     let beams;
     let stave;
@@ -42,13 +73,15 @@ function Stave({
       });
     } else {
       stave = system.addStave({
-        voices: [score.voice(score.notes("B4/1/r"))],
+        voices: [score.voice(score.notes(defaultRest))],
       });
     }
-    if (clef) stave.addClef(clef);
+    if (clef) {
+      score.set({ clef: clef });
+      stave.addClef(clef);
+    }
     if (keySignature) stave.addKeySignature(keySignature);
     if (timeSignature) {
-      score.set({ timeSignature: timeSignature });
       stave.addTimeSignature(timeSignature);
     }
 
